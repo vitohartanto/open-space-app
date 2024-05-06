77be224f0dd201442a3ef455ff3691d4b0b418bb
Vito <vitohartanto@mail.ugm.ac.id>
2024-05-06 23:35:14 +0700

    removing unnecessary comment

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -4,7 +4,6 @@ import LoadingBar from 'react-redux-loading-bar';
 function Loading() {
   return (
     <div className="loading">
-      {/* @TODO: use react-redux-loading-bar to show loading bar */}
       <LoadingBar />
     </div>
   );
